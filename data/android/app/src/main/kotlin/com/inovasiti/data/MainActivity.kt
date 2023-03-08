d99f75ea0789ce0ed3f780ce71460912ce914ecc
mrazal <MohdFadzli.Razali@sicpa.com>
2023-03-09 00:37:10 +0800

    - Created a new project - Created a new data, domain, local, remote & external_plugin modules - update the main pubspec

---
+++ data/android/app/src/main/kotlin/com/inovasiti/data/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inovasiti.data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ external_plugin/android/app/src/main/kotlin/com/inovasiti/external_plugin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inovasiti.external_plugin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
